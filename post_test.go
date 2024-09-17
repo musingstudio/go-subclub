273cc15b6e7f61863af247aecae415a543fa2371
Matt Baer <matt@write.as>
2024-09-17 02:24:04 -0400

    Add post edit test

--- post_test.go
+++ post_test.go
@@ -17,3 +17,20 @@ func TestPost(t *testing.T) {
 
 	t.Logf("Post: %+v", p)
 }
+
+func TestEditPost(t *testing.T) {
+	c := NewClient(os.Getenv("API_KEY"))
+
+	p, err := c.EditPost(&PostUpdateParams{
+		PostID:  os.Getenv("POST_ID"),
+		Content: "Test post (updated)",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if !p.Success {
+		t.Fatalf("Success: %t", p.Success)
+	}
+
+	t.Logf("Updated post: %+v", p)
+}
